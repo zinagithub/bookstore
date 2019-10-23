cb8819bfc6d0e478ceeaf3fb23c61a430886fdf5
Talaa Zina <talazina@gmail.com>
2019-10-23 19:12:39 +0100

    add handleChange method

--- src/containers/BookForm.js
+++ src/containers/BookForm.js
@@ -3,11 +3,21 @@ import React from 'react';
 
 const Categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'] 
 class BookForm extends React.Component {
+	state = {
+		title : '',
+		category : Categories[0],
+	}
+	handleChange = (e) => {
+      this.setState({
+         [e.target.name]: e.target.value
+      });
+	}
 	render(){
 		return (
             <form>
-                  <input name='myInput'  placeholder="Enter the Book name" />
-                  <select name='category'>
+                  <h1>input : {this.state.title} category : {this.state.category}</h1>
+                  <input type = 'text' name='title'  placeholder="Enter the Book name" onChange={this.handleChange}/>
+                  <select name='category' onChange={this.handleChange}>
                     { Categories.map((cat) => (<option  value={cat} key={cat} > {cat} </option>)) }
                   </select>
                   <button>ADD BOOK</button>
